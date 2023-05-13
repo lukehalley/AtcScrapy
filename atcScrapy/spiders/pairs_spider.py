2da491cd627c5ce0b03595ad6d5836e5d99725de
Luke Halley <luke123halley@gmail.com>
2023-05-13 17:15:48 +0100

    getting pairs and their tokens

---
+++ atcScrapy/spiders/pairs_spider.py
@@ -0,0 +1,48 @@
+import csv, os
+import json
+import re
+
+import flatdict
+import scrapy
+
+class PairSpider(scrapy.Spider):
+
+    name = "pair"
+    custom_settings = {
+        'FEEDS': { 'yield/pair.csv': { 'format': 'csv', 'overwrite': True}}
+    }
+    gt_base_url = os.environ["GT_BASE_URL"]
+
+    if os.path.isfile('yield/dex.csv'):
+        with open("yield/dex.csv", "r") as f:
+            reader = csv.DictReader(f)
+            start_urls = [item['dex_url'] for item in reader]
+
+    def parse(self, response, **kwargs):
+
+        pair_network = response.url.replace(self.gt_base_url, "").split("/")[0]
+        pair_dex = response.url.replace(self.gt_base_url, "").split("/")[1]
+
+        reg = r'"fallback".*?}}}}}'
+        extracted_json = "{" + str(re.findall(reg, response.text)[0]).replace('\\"', '')
+        extracted_dict = json.loads(extracted_json)
+        fallback_dict = extracted_dict["fallback"]
+        first_leaf = list(fallback_dict.keys())[0]
+        pair_dicts = fallback_dict[first_leaf]["data"]
+
+        formatted_pair_dicts = []
+        for pair_dict in pair_dicts:
+            formatted_pair_dict = {
+                "pair_name": pair_dict["attributes"]["name"].replace(" ", ""),
+                "pair_network": pair_network,
+                "pair_dex": pair_dex,
+                "pair_address": pair_dict["attributes"]["address"],
+                "pair_volume_in": pair_dict["attributes"]["from_volume_in_usd"],
+                "pair_volume_out": pair_dict["attributes"]["to_volume_in_usd"],
+                "pair_volume_swap": pair_dict["attributes"]["aggregated_network_metrics"][
+                "total_swap_volume_usd_24h"]
+            }
+            formatted_pair_dicts.append(formatted_pair_dict)
+
+        for formatted_pair_dict in formatted_pair_dicts:
+            yield formatted_pair_dict
---
+++ atcScrapy/spiders/token_spider.py
@@ -0,0 +1,49 @@
+import csv, os
+import json
+import re
+
+import flatdict
+import scrapy
+
+class TokenSpider(scrapy.Spider):
+
+    name = "token"
+    custom_settings = {
+        'FEEDS': { 'yield/token.csv': { 'format': 'csv', 'overwrite': True}}
+    }
+    gt_base_url = os.environ["GT_BASE_URL"]
+
+    if os.path.isfile('yield/pair.csv'):
+
+        with open("yield/pair.csv", "r") as f:
+            readFile = csv.DictReader(f)
+            start_urls = [item['pair_tx_url'] for item in readFile]
+
+        full_reader = csv.DictReader(open('yield/pair.csv'))
+        pair_dict = [item for item in full_reader]
+
+    def parse(self, response, **kwargs):
+
+        pair_index = self.start_urls.index(response.url)
+
+        reg = r'<script id="__NEXT_DATA__" type="application\/json">(.*?)<\/script>'
+        extracted_json = re.findall(reg, response.text)[0].replace('\\"', '')
+        extracted_dict = json.loads(extracted_json)
+
+        primary_quote_dict = {}
+
+        primary_token = extracted_dict["props"]["pageProps"]["data"]["token"]["byId"][list(extracted_dict["props"]["pageProps"]["data"]["token"]["byId"].keys())[-1]]
+        primary_quote_dict["primary_token_name"] = primary_token["attributes"]["name"]
+        primary_quote_dict["primary_token_symbol"] = primary_token["attributes"]["symbol"]
+        primary_quote_dict["primary_token_address"] = primary_token["attributes"]["address"]
+        primary_quote_dict["primary_token_decimals"] = primary_token["attributes"]["decimals"]
+        
+        quote_token = extracted_dict["props"]["pageProps"]["data"]["token"]["byId"][list(extracted_dict["props"]["pageProps"]["data"]["token"]["byId"].keys())[0]]
+        primary_quote_dict["quote_token_name"] = quote_token["attributes"]["name"]
+        primary_quote_dict["quote_token_symbol"] = quote_token["attributes"]["symbol"]
+        primary_quote_dict["quote_token_address"] = quote_token["attributes"]["address"]
+        primary_quote_dict["quote_token_decimals"] = quote_token["attributes"]["decimals"]
+
+        pairs_dict_merged = primary_quote_dict | self.pair_dict[pair_index]
+
+        yield pairs_dict_merged
