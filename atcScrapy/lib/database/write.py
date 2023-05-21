d57a2b98a3ba559dd9b4f0d95e1a712a2e3aafcd
Luke Halley <luke123halley@gmail.com>
2023-05-21 19:42:48 +0100

    getting txs

--- atcScrapy/lib/database/write.py
+++ atcScrapy/lib/database/write.py
@@ -1,6 +1,5 @@
 from atcScrapy.lib.database.connect import create_db_connection
 
-
 def execute_db_insert(query_table_name, query_keys, query_values, update_on_exists=True):
 
     db_connection = create_db_connection()
@@ -19,6 +18,34 @@ def execute_db_insert(query_table_name, query_keys, query_values, update_on_exis
         query_str = f"INSERT {query_str}"
 
     db_cursor.execute(query_str, query_values)
+
+    db_connection.commit()
+
+    db_connection.close()
+
+def execute_db_update(query_table_name, update_dict, where_dict):
+
+    db_connection = create_db_connection()
+    db_cursor = db_connection.cursor()
+
+    query = f"UPDATE " \
+                 f"{query_table_name} " \
+                 f"SET"
+
+    for index, (key, value) in enumerate(update_dict.items()):
+        query = f"{query} {query_table_name}.{key} = {value}"
+        if index + 1 < len(update_dict):
+            query = f"{query},"
+
+    query = f"{query} " \
+                 f"WHERE"
+
+    for index, (key, value) in enumerate(where_dict.items()):
+        query = f"{query} {key} = {value}"
+        if index + 1 < len(update_dict):
+            query = f"{query} AND"
+
+    db_cursor.execute(query)
 
     db_connection.commit()
 
